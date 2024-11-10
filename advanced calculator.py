0be74197ab030011950b3948f39ce5c927b416c5
mmir17 <mahammir.ca@gmail.com>
2024-11-10 15:59:04 -0700

    added operation functions comments

--- advanced calculator.py
+++ advanced calculator.py
@@ -1,22 +1,26 @@
 # create the functions for the operations:
 
+# function for adding two values together
 def add(x, y):
     return x + y
 
 
+# function for subtracting two values
 def subtract(x, y):
     return x - y
 
 
+# function for multiplying two values
 def multiply(x, y):
     return x * y
 
 
+# function for dividing two values
 def divide(x, y):
     if y != 0:
         return x / y
     else:
-        print('error!')
+        print('error!')  # condition for dividing by zero
 
 
 user_input = input("Please enter the equation you would like to solve:")
