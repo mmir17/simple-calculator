b0a04033d34a8fba3e305a8acdf0f291d4b29ee5
mmir17 <mahammir.ca@gmail.com>
2024-11-10 17:30:42 -0700

    final version

--- advanced calculator.py
+++ advanced calculator.py
@@ -1,5 +1,4 @@
 # create the functions for the operations:
-
 # function for adding two values together
 def add(x, y):
     return x + y
@@ -23,13 +22,15 @@ def divide(x, y):
         print('error!')  # condition for dividing by zero
 
 
+#   take user input
 user_input = input("Please enter the equation you would like to solve: ")  # get equation
 split_input = enumerate(user_input.split())  # split equation at every space
-print(split_input)
+#   give index and value for each split part
 
 number = []
 operation = []
 
+#   separate the numbers vs operations
 for i, num in split_input:
     if i % 2 == 0:  # if index evenly divisible by 2, then it's a number
         num = int(num)  # turn number into an integer
@@ -37,22 +38,18 @@ for i, num in split_input:
     else:
         operation.append(num)  # otherwise append to the list of operations
 
-print(number)
-print(operation)
-for i in range(len(number)):  # iterate through the numbers
-    n1 = number[0]  # first number
-    n2 = number[1]  # second number
-    for j in range(len(operation)):  # iterate through the operations
-        opp = operation[0]
-        if opp == "+":
-            n1 = add(n1, n2)
-        elif opp == "-":
-            result = subtract(n1, n2)
-        elif opp == "*":
-            n1 = multiply(n1, n2)
-        elif opp == "/":
-            n1 = divide(n1, n2)
-        j += 1
-    i += 2
+#   calculate
+n1 = number[0]  # first number
+for i in range(len(operation)):  # iterate through the operations
+    n2 = number[i + 1]  # second number
+    opp = operation[i]  # current operation
+    if opp == "+":
+        n1 = add(n1, n2)
+    elif opp == "-":
+        n1 = subtract(n1, n2)
+    elif opp == "*":
+        n1 = multiply(n1, n2)
+    elif opp == "/":
+        n1 = divide(n1, n2)
 
 print(n1)  # print answer
